21cfb366861d4c447608acc00db5de2a5863f110
Janay Ma <janay.ma7@gmail.com>
2023-08-09 13:41:33 -0700

    implement parametrized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,21 +6,26 @@ const pool = new Pool({
   database: 'janayma'
 });
 
-const args = process.argv.slice(2);
-//console.log(args);
-pool.query(`
+
+const cohortName = process.argv[2] || 'JUL02';
+const limit = process.argv[3] || 2;
+const values = [`${cohortName}`, limit];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers 
 JOIN assistance_requests on assistance_requests.teacher_id = teachers.id
 JOIN students on assistance_requests.student_id = students.id
 JOIN cohorts on students.cohort_id = cohorts.id
-WHERE cohorts.name = '${args[0] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then( res => {
-  //console.log(res.rows)
-  res.rows.forEach(teacher => {
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+LIMIT $2;
+`
+pool.query(queryString, values)
+  .then( res => {
+    //console.log(res.rows)
+    res.rows.forEach(teacher => {
 
-    console.log(`${teacher.cohort}: ${teacher.teacher}`);
+      console.log(`${teacher.cohort}: ${teacher.teacher}`);
+    })
   })
-})
